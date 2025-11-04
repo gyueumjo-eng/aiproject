2fd44bfb87933ebcd122586d112a66409d3d56cd
gyueumjo-eng <gyueumjo@gmail.com>
2025-11-04 14:08:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 st.title('나의 첫 웹페이지')
-name=st.text_input('이름을 입력하시오!')
-st.selectbox('좋아하는 음식을 입력해주세요',['치킨','떡볶이','바나나'])
+name=st.text_input('이름을 입력하시어유!')
+m=st.selectbox('좋아하는 음식을 선택해주시어유~',['치킨','떡볶이','바나나'])
 if st.button('인삿말 생성'):
-  st.writte(name+'님! 안녕하세요!')
+  st.info(name+'님! 안녕하시어유!')
+st.warning(m+'을(를)좋아하시나봐유?, 저도좋아혀유~')
+st.error(반가워유~')
